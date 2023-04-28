88bc5d1799b3c84b779c525c180c2bc878f87fde
Sasho878748 <kovalenko878748@gmail.com>
2023-04-28 15:42:05 +0300

    + _.throttle task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,16 +2,20 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 
+const throttle = require('lodash.throttle');
+
 const player = new Player('vimeo-player');
 
-player.on('timeupdate', function (data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
-  
-});
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
 
-window.addEventListener('load', function() {
-    const currentTime = localStorage.getItem('videoplayer-current-time');
-    if (currentTime !== null) {
-      player.setCurrentTime(currentTime);
-    }
-  });
+window.addEventListener('load', function () {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime !== null) {
+    player.setCurrentTime(currentTime);
+  }
+});
