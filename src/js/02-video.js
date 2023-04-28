bcd55ae45d7527146692d4e48c8656ebf11f3542
Sasho878748 <kovalenko878748@gmail.com>
2023-04-28 15:24:17 +0300

    finish task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player('vimeo-player');
+
+player.on('timeupdate', function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+  
+});
+
+window.addEventListener('load', function() {
+    const currentTime = localStorage.getItem('videoplayer-current-time');
+    if (currentTime !== null) {
+      player.setCurrentTime(currentTime);
+    }
+  });
